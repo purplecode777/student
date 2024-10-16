5b87ef61f7595d02e6c0f04689f76e1bf487c825
purplecode777 <130860111+purplecode777@users.noreply.github.com>
2024-10-17 01:03:41 +0530

    Added basic python practice programs

---
+++ demo.py
@@ -0,0 +1,43 @@
+print("Hello World")
+
+#add two integers
+a = 2
+b = 3
+c = a+b
+print(c)
+
+#To print even number using if-else condition
+a = 12
+if a%2==0:
+    print("The number is even number")
+else:
+    print("The number is odd number")
+
+#list
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+#set
+set ={"school","study","books"}
+print(set)
+
+#dictionary
+dict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(dict)
+
+#algebraic operations
+a = 5
+b = 7
+print(a+b)
+print(a-b)
+print(a*b)
+
+#index number of a string
+str = "Good morning have a nice day"
+a = str.index("m")
+print(a)
